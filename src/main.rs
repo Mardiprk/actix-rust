063d755ce9761da79498e246789def5a8d5d24de
Mardiprk <mk1769770@gmail.com>
2025-07-06 14:14:34 +0530

    actix post

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
 use actix_web::{web::{self, get}, App, HttpResponse, HttpServer, Result};
+use serde::Deserialize;
 
-async fn hello() -> Result<HttpResponse>{
-    Ok(HttpResponse::Ok().body("Learning Actix"))
+#[derive(Deserialize)]
+struct GreetRequest{
+    name: String
 }
 
-async  fn about(name: web::Path<String>) -> Result<HttpResponse>{
+async fn hello() -> Result<HttpResponse>{
+    Ok(HttpResponse::Ok().body("Hello, Actix"))
+}
+
+async fn about(name: web::Path<String>) -> Result<HttpResponse>{
     Ok(HttpResponse::Ok().body(format!("{} wants to know about Actix", name)))
+}
+
+async fn greet(req: web::Json<GreetRequest>) -> Result<HttpResponse>{
+    Ok(HttpResponse::Ok().body(format!("Hello! {}", req.name)))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
-    println!("Starting Running in http://localhost:8080");
+    println!("Running server @ http://localhost:8080");
 
     HttpServer::new(||{
         App::new()
         .route("/", get().to(hello))
         .route("/about/{name}", get().to(about))
-    })
-    .bind("127.0.0.1:8080")?
+        .route("/greet", web::post().to(greet))
+    }).bind("127.0.0.1:8080")?
     .run()
     .await
 }
